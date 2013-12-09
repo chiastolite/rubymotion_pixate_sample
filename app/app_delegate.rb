958ae53ee47896b83bad1b36415948f742f88baf
chiastolite <chiastolite.1980@gmail.com>
2013-12-10 01:13:06 +0900

    pixate demo

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    @blue_view.styleClass = 'blue'
+    @window.addSubview(@blue_view)
+
     true
   end
 end
